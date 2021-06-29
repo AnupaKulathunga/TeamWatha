11767e220ec388385f06174bce10ec9c2f96849d
AnupaKulathunga <pmadnkulathunga@gmail.com>
2021-06-29 16:20:03 +0530

    'a'

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
 
+
+
 available_indicators = df['Indicator Name'].unique()
 
 app.layout = html.Div([
